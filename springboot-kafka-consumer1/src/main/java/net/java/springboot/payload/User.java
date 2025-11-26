e619569b21a57e0ecfdf9ed28922a4fdb9ab23fd
vivekm2008 <mahajanv2008@gmail.com>
2025-11-26 13:06:10 +0530

    Update User.java

--- springboot-kafka-consumer1/src/main/java/net/java/springboot/payload/User.java
+++ springboot-kafka-consumer1/src/main/java/net/java/springboot/payload/User.java
@@ -7,11 +7,9 @@ public class User {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getFirstName() {
         return firstName;
     }
